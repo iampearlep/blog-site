a9fa92fd626cd8366244cc6c1dfd628669648fa6
iampearlep <damilolaprincess4@gmail.com>
2024-02-03 19:15:35 +0100

    fix: add suspense

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,11 +1,15 @@
 "use client"
+import { Suspense } from 'react'
 import SearchResult from "@/components/SearchResult"
 import Cta from "@/components/Cta"
 export default function Page(){
     return(
         <main>
              <h1 className="text-3xl text-center font-semibold py-7">Search Results...</h1>
-           <SearchResult />
+             <Suspense fallback={<p>Loading feed...</p>}>
+             <SearchResult />
+            </Suspense>
+          
         </main>
     )
 }
